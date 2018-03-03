c50a1d221b34131cee0938ab719001f21a24f70e
Joey Zhou <josephyzhou@gmail.com>
2018-03-02 22:38:36 -0500

    2018-03-02

--- report/report.go
+++ report/report.go
@@ -1,39 +1,95 @@
 package report
 
 import (
-	"fmt"
+	"bufio"
+	"os"
 	"sort"
 
 	coinMarketCap "github.com/cryptocurrencyfund/go-coinmarketcap"
 )
 
-// Report Report
-type Report struct {
-	Winners       []coinMarketCap.Coin
-	Losers        []coinMarketCap.Coin
-	VolumeWinners []coinMarketCap.Coin
-	VolumeLosers  []coinMarketCap.Coin
+// Generate Generate daily report
+func Generate(dateString string, top []coinMarketCap.Coin) {
+	filename := dateString + ".md"
+	createMarkDown(dateString, filename)
+	priceChangeMd(filename, top)
+	volMd(filename, top)
 }
 
-// Generate generate daily report
-func Generate(dateString string, top []coinMarketCap.Coin) {
-	var r Report
+func createMarkDown(date string, filename string) {
+
+	// open output file
+	fo, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	// close fo on exit and check for its returned error
+	defer func() {
+		if err := fo.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	// make a write buffer
+	w := bufio.NewWriter(fo)
+	w.WriteString("### " + date + "\n")
+	w.Flush()
+}
+
+func priceChangeMd(filename string, top []coinMarketCap.Coin) {
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
 	sort.Slice(top, func(i, j int) bool {
 		return top[i].PercentChange24h > top[j].PercentChange24h
 	})
-	copy(r.Winners, top[:10])
-	copy(r.Losers, top[len(top)-10:])
+	winners := top[:10]
+	losers := top[len(top)-10:]
+
+	str := "\n#### Price Change Winners\n"
+	for _, w := range winners {
+		str += w.MarkdownPrice() + "\n"
+	}
+	str += "\n#### Price Change Losers\n"
+	for _, l := range losers {
+		str += l.MarkdownPrice() + "\n"
+	}
+
+	if _, err = f.WriteString(str); err != nil {
+		panic(err)
+	}
+}
+
+func volMd(filename string, top []coinMarketCap.Coin) {
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
 
 	sort.Slice(top, func(i, j int) bool {
 		return top[i].Usd24hVolume > top[j].Usd24hVolume
 	})
-	copy(r.VolumeWinners, top[:10])
-	copy(r.VolumeLosers, top[len(top)-10:])
+	volumeWinners := top[:10]
+	volumeLosers := top[len(top)-10:]
+	str := "\n#### 24H Volume Winners\n"
+	for _, w := range volumeWinners {
+		str += w.MarkdownPrice() + "\n"
+	}
+	str += "\n#### 24H Volume Losers\n"
+	for _, l := range volumeLosers {
+		str += l.MarkdownPrice() + "\n"
+	}
 
-	fmt.Printf("Winners: \n%v\n", r.Winners)
-	fmt.Printf("Losers: \n%v\n", r.Winners)
-	fmt.Printf("Volumn winners: \n%v\n", r.VolumeWinners)
-	fmt.Printf("Volumn losers: \n%v\n", r.VolumeLosers)
+	if _, err = f.WriteString(str); err != nil {
+		panic(err)
+	}
 }
 
 // GeneratePortfolio generate portfolio

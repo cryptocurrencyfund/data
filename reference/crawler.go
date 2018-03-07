480f2f04cdc7c38191af147efbd352465297920a
Joey Zhou <josephyzhou@gmail.com>
2018-03-07 10:02:53 -0500

    2018-03-07

--- reference/crawler.go
+++ reference/crawler.go
@@ -21,7 +21,7 @@ func CrawlHistoricalData(date string, top []coinMarketCap.Coin) {
 
 // CrawlCurrency CrawlCurrency
 func CrawlCurrency(date string, currency string) {
-	filename := "reference/" + currency + ".csv"
+	filename := "reference/historical/" + currency + ".csv"
 	file, err := os.Create(filename)
 	if err != nil {
 		log.Fatalf("Cannot create file %q: %s\n", filename, err)
@@ -52,5 +52,40 @@ func CrawlCurrency(date string, currency string) {
 	url := fmt.Sprintf("https://coinmarketcap.com/currencies/%s/historical-data/?start=20110101&end=%s",
 		currency,
 		end)
+	c.Visit(url)
+}
+
+// CrawlCoinInfo CrawlCoinInfo
+func CrawlCoinInfo(currency string) {
+	filename := "reference/coinInfo/" + currency + ".csv"
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatalf("Cannot create file %q: %s\n", filename, err)
+		return
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write CSV header
+	writer.Write([]string{"Date", "Open", "High", "Low", "Close", "Volume", "Market Cap"})
+
+	// Instantiate default collector
+	c := colly.NewCollector()
+
+	c.OnHTML(".list-unstyled li", func(e *colly.HTMLElement) {
+		// fmt.Println("\n\n" + e.ChildText(""))
+		writer.Write([]string{
+			e.ChildText("td:nth-child(1)"),
+			e.ChildText("td:nth-child(2)"),
+			e.ChildText("td:nth-child(3)"),
+			e.ChildText("td:nth-child(4)"),
+			e.ChildText("td:nth-child(5)"),
+			e.ChildText("td:nth-child(6)"),
+			e.ChildText("td:nth-child(7)"),
+		})
+	})
+	url := fmt.Sprintf("https://coinmarketcap.com/currencies/%s",
+		currency)
 	c.Visit(url)
 }
